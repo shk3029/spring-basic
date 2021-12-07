a83bf805403048aa4161e46adc94f80b4917461d
shk3029 <shk30290@naver.com>
2021-12-07 20:46:03 +0900

    feat: component scan (2)

--- src/main/java/hello/core/springbasic/AutoAppConfig.java
+++ src/main/java/hello/core/springbasic/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //basePackages = "hello.core.springbasic",
+        // default : 현재 클래스 패키지 위치 하위로 컴포넌트 스캔을 함
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
