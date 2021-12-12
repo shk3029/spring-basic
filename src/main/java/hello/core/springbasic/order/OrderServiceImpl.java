708065bc53288af6400b99ba173f690b51ceb410
shk3029 <shk30290@naver.com>
2021-12-12 19:29:43 +0900

    feat: 다양한 의존관계 주입 방법

--- src/main/java/hello/core/springbasic/order/OrderServiceImpl.java
+++ src/main/java/hello/core/springbasic/order/OrderServiceImpl.java
@@ -3,7 +3,6 @@ package hello.core.springbasic.order;
 import hello.core.springbasic.discount.DiscountPolicy;
 import hello.core.springbasic.member.Member;
 import hello.core.springbasic.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +11,6 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
