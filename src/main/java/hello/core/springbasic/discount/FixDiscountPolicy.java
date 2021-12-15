66c928868d9ef5f9b9e6544a51aa6d92fcc355b0
shk3029 <shk30290@naver.com>
2021-12-15 23:46:49 +0900

    feat: @Autowired 필드명, @Qualifier

--- src/main/java/hello/core/springbasic/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/springbasic/discount/FixDiscountPolicy.java
@@ -2,7 +2,11 @@ package hello.core.springbasic.discount;
 
 import hello.core.springbasic.member.Grade;
 import hello.core.springbasic.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
