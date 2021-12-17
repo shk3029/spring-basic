e45b5c433989f6440bc25a067efc1654a4d3ce4c
jongseon <shc.jsjeong@gmail.com>
2021-12-17 12:26:34 +0900

    feat: 빈 생명주기 콜백 시작

---
+++ src/test/java/hello/core/springbasic/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package hello.core.springbasic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    // 객체의 생성과 초기화 로직은 분리하자
+    // 값 셋팅 같은 경우만 생성자에 넣고, 연결 및 무거운 기능은 초기화에서 하자
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello");
+            return networkClient;
+        }
+
+    }
+}
