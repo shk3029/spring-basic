7d76c30094f6e01b1075b0dc7398b25550f86538
shk3029 <shk30290@naver.com>
2021-12-28 16:47:02 +0900

    feat: request scope 예제

---
+++ src/main/java/hello/core/springbasic/web/LogDemoController.java
@@ -0,0 +1,42 @@
+package hello.core.springbasic.web;
+
+import hello.core.springbasic.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestUrl = request.getRequestURL().toString();
+        myLogger.setRequestUrl(requestUrl);
+
+        myLogger.log( );
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/hello/core/springbasic/web/LogDemoService.java
@@ -0,0 +1,7 @@
+package hello.core.springbasic.web;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogDemoService {
+}
